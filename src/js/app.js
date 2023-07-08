31026869d1aff1d6c152ea6a3f0a21e5abab93f1
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-07-08 14:21:26 +0500

    fixed bugs

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,19 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+window.addEventListener('load', function() {
+    var header = document.querySelector('header');
+    
+    if (window.pageYOffset > 0) {
+        header.classList.add('fixed');
+    }
+  
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 0) {
+            header.classList.add('fixed');
+        } else {
+            header.classList.remove('fixed');
+        }
+    });
+});
